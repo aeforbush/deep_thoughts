2bfa1c5a59b3dea5a3e822a9482a0b8638f00910
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-12-10 16:26:03 -0700

    rendering data

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React from "react";
 // Provider sends data to all other components, Client is a constructor function/connect to GraphQL server, Cache response data for efficiency, Link controls how Client makes a req
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-import Home from './pages/Home';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+import SingleThought from "./pages/SingleThought";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 // this creates graphql endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 // this creates Apollo Client instance and connection to graphql API endpoint
@@ -18,17 +27,32 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+// from snapcode
+// const client = new ApolloClient({
+//   uri: '/graphql'
+// });
+
 // passing the client variable in as the value for the client prop, everything between JSX tags will have access to the server's API data through client we set up.
 function App() {
   return (
     <ApolloProvider client={client}>
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
-      </div>
-      <Footer />
-    </div>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const ThoughtList = ({ thoughts, title }) => {
   if (!thoughts.length) {
@@ -11,15 +12,23 @@ const ThoughtList = ({ thoughts, title }) => {
         thoughts.map((thought) => (
           <div key={thought._id} className="card mb-3">
             <p className="card-header">
+              <Link
+              to={`/profile/${thought.username}`}
+              style={{ fontWeight: 700 }}
+              className="text-light"
+              >
               {thought.username}
+              </Link>{' '}
               thought on {thought.createAt}
             </p>
-            <div className="mb-0">
+            <div className="card-body">
+              <Link to={`/thought/${thought.id}`}>
               <p>{thought.thoughtText}</p>
               <p className="mb-0">
                 Reactions: {thought.reactionCount} || Click to{" "}
                 {thought.reactionCount ? "see" : "start"} the discussion!
               </p>
+              </Link>
             </div>
           </div>
         ))}
@@ -28,3 +37,4 @@ const ThoughtList = ({ thoughts, title }) => {
 };
 
 export default ThoughtList;
+// In both instances, we've added an extra path to the end of the Link component. For example, /profile/${thought.username} would become /profile/Zoe66 for that particular user.
