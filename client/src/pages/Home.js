60f33373ab9da746dd2cb9b0d5569698f9fd47cc
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-12-11 11:06:44 -0700

    added add thought mutation

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+
 import ThoughtList from "../components/ThoughtList";
 import FriendList from "../components/FriendList";
+import ThoughtForm from "../components/ThoughtForm";
 
 import { useQuery } from "@apollo/client";
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from "../utils/queries";
+
 import Auth from "../utils/auth";
 
 // when loading execute query for thought data (async), when finished data is stored in the destructured data property
@@ -25,6 +28,11 @@ const Home = () => {
   return (
     <main>
       <div className="flex-row justify-space-between">
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
+          </div>
+        )}
         <div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}>
           {loading ? (
             <div>Loading...</div>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,15 +3,20 @@ import { Redirect, useParams } from "react-router-dom";
 
 import ThoughtList from "../components/ThoughtList";
 import FriendList from "../components/FriendList";
+import ThoughtForm from '../components/ThoughtForm';
 
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import { ADD_FRIEND } from '../utils/mutations';
+
 
 import Auth from "../utils/auth";
 
 // The useParams Hook retrieves the username from the URL, which is then passed to the useQuery Hook.
 
 const Profile = (props) => {
+  const [addFriend] = useMutation(ADD_FRIEND);
+
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
@@ -37,12 +42,25 @@ const Profile = (props) => {
     );
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
+        {userParam && (
+        <button className="btn ml-auto" onClick={handleClick}>Add Friend</button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -61,6 +79,7 @@ const Profile = (props) => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
